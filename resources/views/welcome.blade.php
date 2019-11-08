86f67997e2514f0919ba6e25ffc264166aab3c9c
Gezim Gjoklaj <>
2019-11-08 11:13:10 +0100

    WelcomeBlade created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Immoweb</title>
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -13,13 +14,14 @@
         <style>
             html, body {
                 background-color: #fff;
-                color: #636b6f;
+                background-image: url("Startseite.jpeg");
+                background-size: 100%;
+                color: #ffffff;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
@@ -27,7 +29,8 @@
             .flex-center {
                 align-items: center;
                 display: flex;
-                justify-content: center;
+                justify-content: left;
+                margin-left: 40px;
             }
 
             .position-ref {
@@ -46,13 +49,25 @@
 
             .title {
                 font-size: 84px;
+                text-align: left;
+                margin-left: 18px;
             }
 
             .links > a {
-                color: #636b6f;
+                color: #ffffff;
                 padding: 0 25px;
                 font-size: 13px;
-                font-weight: 600;
+                font-weight: 500;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .options > a {
+                color: #8b4513;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 700;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
@@ -66,32 +81,26 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="top-right options">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                      @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Immoweb
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">Mieter</a>
+                    <a href="https://laracasts.com">Wohnungen</a>
+                    <a href="https://laravel-news.com">Verträge</a>
+                    <a href="https://blog.laravel.com">Rechnungen</a>
+                    <a href="https://nova.laravel.com">Overview</a>
                 </div>
             </div>
         </div>
