d46d2af0a2b26defca1518d6b12fa40a1c6ff621
Luzius Prantl <luzius.prantl@aerztekasse.ch>
2019-11-19 12:28:34 +0100

    added crud contracts

--- app/Http/Controllers/ContractsController.php
+++ app/Http/Controllers/ContractsController.php
@@ -12,4 +12,40 @@ class ContractsController extends Controller
                return view('contracts', ['contracts' => $contracts]);
 
        }
+
+
+
+
+        public function addContract($startDate, $terminationDate, $rentPerMonth, $tenantMapFk, $appartmentFk){
+        $id = DB::table('contract')->insertGetId(
+
+         ['startDate' => $startDate],
+         ['terminationDate' => $terminationDate],
+         ['isActive' => true],
+         ['rentPerMonth' => $rentPerMonth],
+         ['tenantMapFk' => $tenantMapFk],
+         ['appartmentFk' => $appartmentFk]
+
+         );
+
+        }
+
+        public function deleteContract($id){
+        DB::table('contract')->where('id', '=', $id)->update(['isActive' => false]);
+
+        }
+
+        public function updateContract($id, $startDate, $tenantMapFk, $appartmentFk){
+                             DB::table('contract')
+                                         ->where('id', $id)
+                                         ->update(
+
+         ['startDate' => $startDate ],
+         ['terminationDate' => $terminationDate],
+         ['rentPerMonth' => $rentPerMonth],
+         ['tenantMapFk' => $tenantMapFk],
+         ['appartmentFk' => $appartmentFk]
+
+                                           );
+          }
 }
