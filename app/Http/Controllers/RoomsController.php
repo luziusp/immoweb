496c02d4b0c302ac966b793fc9cafdfec93b631d
Luzius Prantl <luzius.prantl@aerztekasse.ch>
2019-11-19 15:48:29 +0100

    moved logic from controllers to models

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -13,35 +13,6 @@ class RoomsController extends Controller
                        return view('rooms', ['rooms' => $rooms]);
         }
 
-        public function addRoom($appartmentName, $noOfRooms, $squareMeters, $Description, $rentCost, $additionalCost){
-        $id = DB::table('appartment')->insertGetId(
-
-         ['appartmentName' => $appartmentName],
-         ['noOfRooms' => $noOfRooms],
-         ['squareMeters' => $squareMeters],
-         ['Description' => $Description],
-         ['rentCost' => $rentCost],
-         ['additionalCost' => $additionalCost],
-         ['isActive' => true]
-         );
-
-        }
-        public function deleteRoom($id){
-        DB::table('appartment')->where('id', '=', $id)->update(['isActive' => false]);
-
-        }
-        public function updateRoom($id, $appartmentName, $noOfRooms, $squareMeters, $Description, $rentCost, $additionalCost){
-                             DB::table('appartment')
-                                         ->where('id', $id)
-                                         ->update(
-
-                                           ['appartmentName' => $appartmentName ],
-                                           ['noOfRooms' => $noOfRooms],
-                                           ['squareMeters' => $squareMeters],
-                                           ['Description' => $Description],
-                                           ['rentCost' => $rentCost],
-                                           ['additionalCost' => $additionalCost]
-                                           );
-          }
+       
 
 }
